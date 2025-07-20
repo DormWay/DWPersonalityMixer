ac81936d7967b733a7cd5654832f2410df30b88b
Ethan Kaplan <ethankap@murmurs.com>
2025-07-20 04:54:00 -0700

    Initial Commit

---
+++ DWQuadPreference/DWQuadPreferenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DWQuadPreferenceApp.swift
+//  DWQuadPreference
+//
+//  Created by Ethan Kaplan on 7/20/25.
+//
+
+import SwiftUI
+
+@main
+struct DWQuadPreferenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
